0ae8bf8b9771bac63bae000c0451cc2f90135ef9
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-10-14 20:34:03 +0530

    hero or sahin section add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Explore from "./components/Explore";
 import WhyUs from "./components/WhyUs";
+import Hero from "./components/homepage/Hero";
 
 function App() {
   return (
     <>
+    <Hero/>
       <WhyUs />
       <Explore />
     </>
