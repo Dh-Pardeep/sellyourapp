6641c4ebbb940123bee3b861c9f13c232dcc7c67
sachin-niyana <sachinjakhar100@gmail.com>
2023-10-14 20:04:56 +0530

    addsecondsection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Explore from "./components/Explore";
 import WhyUs from "./components/WhyUs";
 
 function App() {
   return (
     <>
       <WhyUs />
+      <Explore />
     </>
   );
 }
